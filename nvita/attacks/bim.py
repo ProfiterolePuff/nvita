1de1ee1af689890a53b12088592f534ce8139a1a
Zeyu Chen <markchenflm@gmail.com>
2022-07-05 23:16:28 +1200

    Implementation of gradient-based adversarial TSF attacks

--- nvita/attacks/bim.py
+++ nvita/attacks/bim.py
@@ -1,11 +1,70 @@
+import torch
 
-class TSBIM:
+from nvita.models.train import predict
+
+class BIMTSF:
     """
-    Time-series BIM.
+    BIM for time series forecasting
     """
 
-    def __init__(self, targeted=False) -> None:
-        pass
+    def __init__(self, eps, alpha, steps, model, loss_type = "MSE", targeted=False) -> None:
+        self.eps = eps
+        self.alpha = alpha
+        self.steps = steps
+        self.model = model
 
-    def generate(self, X, y=None):
-        return X
+        if loss_type == "MSE":
+            self.criterion = torch.nn.MSELoss(reduction = "mean")
+        elif loss_type == "L1":
+            self.criterion = torch.nn.L1Loss(reduction = "mean")
+        else:
+            raise Exception("Loss function" + str(loss_type) + "is not \"MSE\" or \"L1\"")
+
+        self.targeted = targeted
+
+    def attack(self, X, target, window_range):
+        """
+        For non-targeted attack, target is ground truth y
+        For targeted attack, target is (i, g)
+        """
+        X_adv = torch.clone(X).detach()
+        X_adv.requires_grad = True
+        min_X_adv = X - self.eps * window_range
+        max_X_adv = X + self.eps * window_range
+        y_pred = predict(self.model, X_adv)
+
+        if self.targeted:
+
+            target_value = torch.tensor(target[1]).reshape(y_pred.shape)
+
+        else:
+
+            loss = self.criterion(y_pred, target)
+
+        for _ in range(self.steps):
+
+            y_pred = self.model.predict(X_adv)
+            self.model.zero_grad()
+
+            if self.targeted:
+
+                loss = self.criterion(y_pred, target_value)
+            else:
+
+                loss = self.criterion(y_pred, target)
+            
+            loss.backward()
+            data_grad = X_adv.grad.data
+            sign_data_grad = data_grad.sign()
+
+            if self.targeted:
+
+                X_adv = torch.clamp(X_adv - self.alpha * sign_data_grad * window_range, min_X_adv, max_X_adv).detach()
+                
+            else:
+
+                X_adv = torch.clamp(X_adv + self.alpha * sign_data_grad * window_range, min_X_adv, max_X_adv).detach()
+
+            X_adv.requires_grad = True
+
+        return X_adv
--- nvita/attacks/fgsm.py
+++ nvita/attacks/fgsm.py
@@ -1,11 +1,54 @@
+import torch
 
-class TSFGSM:
+from nvita.models.train import predict
+
+class FGSMTSF:
     """
-    Time-series FGSM.
+    FGSM for time series forecasting
     """
 
-    def __init__(self, targeted=False) -> None:
-        pass
+    def __init__(self, eps, model, loss_type = "MSE", targeted=False) -> None:
+        self.eps = eps
+        self.model = model
 
-    def generate(self, X, y=None):
-        return X
+        if loss_type == "MSE":
+            self.criterion = torch.nn.MSELoss(reduction = "mean")
+        elif loss_type == "L1":
+            self.criterion = torch.nn.L1Loss(reduction = "mean")
+        else:
+            raise Exception("Loss function" + str(loss_type) + "is not \"MSE\" or \"L1\"")
+
+        self.targeted = targeted
+
+    def attack(self, X, target, window_range):
+        """
+        For non-targeted attack, target is ground truth y
+        For targeted attack, target is (i, g)
+        """
+        X_adv = torch.clone(X).detach()
+        X_adv.requires_grad = True
+        y_pred = predict(self.model, X_adv)
+
+        if self.targeted:
+
+            target_value = torch.tensor(target[1]).reshape(y_pred.shape)
+            loss = self.criterion(y_pred, target_value)
+            
+        else:
+
+            loss = self.criterion(y_pred, target)
+            
+        self.model.zero_grad()
+        loss.backward()
+        data_grad = X_adv.grad.data
+        sign_data_grad = data_grad.sign()
+
+        if self.targeted:
+
+            X_adv = X_adv - self.eps * sign_data_grad * window_range
+
+        else:
+
+            X_adv = X_adv + self.eps * sign_data_grad * window_range
+
+        return X_adv
