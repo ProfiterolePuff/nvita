ba40a804292b00a91c2d681e1ced54201872ac78
Zeyu Chen <markchenflm@gmail.com>
2022-07-11 10:08:59 +1200

    Add acf plots for the first 4 dataset

--- setup.py
+++ setup.py
@@ -12,6 +12,6 @@ setup(
     name='nvita',
     description='nVITA -- The adversarial attack algorithm for time series forecasting',
     packages=find_packages(),
-    version='0.0.2',
+    version='0.0.3',
     python_requires='>=3.9',
 )
