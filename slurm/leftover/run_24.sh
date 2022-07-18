0ae728a463c5b3754da79e7bd29f466663a1e71a
Luke Chang <luke.x.chang@gmail.com>
2022-07-19 00:29:34 +1200

    fix output log name

--- slurm/leftover/run_24.sh
+++ slurm/leftover/run_24.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATSH --job-name=nvita_leftover
-#SBATCH --output=log/log_%%x_%%j_%%a.out
-#SBATCH --error=log/log_%%x_%%j_%%a.err
+#SBATCH --output=log/log_%x_%j_%a.out
+#SBATCH --error=log/log_%x_%j_%a.err
 #SBATCH --time=72:00:00
 #SBATCH --mem=8G
 #SBATCH --cpus-per-task=6
